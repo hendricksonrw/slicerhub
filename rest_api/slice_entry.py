aeba8a36bc4f0734db6a7de70c8b78b6afe79071
Ross Hendrickson <ross.hendrickson@gmail.com>
2013-01-27 16:00:50 -0700

    Stubbed in most of the architecture and models

--- rest_api/slice_entry.py
+++ rest_api/slice_entry.py
@@ -6,7 +6,7 @@ from  bottle import run, route, static_file, request
 def serve_index():
 	return static_file('index.html', os.getcwd())
 
-@route('/slice', method='POST')
+@route('/slices', method='POST')
 def slice_model():
 	from slicebase import SliceBase
 	from bottle import request
@@ -28,6 +28,51 @@ def slice_model():
 def serve_gcode_by_slice_id(slice_id=''):
 	if slice_id != '':
 			return static_file()
+
+"""
+Start jobs
+POST /slices
+Writes stl and config into /id_number/ folder
+Adds a task to the task queue to process the given ID (verify if we can pass raw data into the queue)
+Requires Header that details what API version to use
+param for which slicer to use
+param for priority level (will default to API Key Priority)
+responds with 
+200 + id number
+401 - not authenticated
+400 - if we’re missing which slicer header information (API defaults to latest rev)
+
+Process job 
+GET /slices/<id number>/ -> JSON from db
+	200 - Return JSON
+		404 - ID didn’t exist in DB
+			401 - Not authenticated
+			PUT /slices/<id number>/
+			With JSON that includes statistics that go into the DB for time to slice
+			200 - JSON inserted into DB
+			400 - If JSON poorly formed
+			404 - If <id number> doesn’t exist
+			401 - Not authenticated
+			5** - If we run into a server error
+			PUT /slices/<id_number>/gcode
+			Posts the created Gcode to be written
+			200 - Gcode was written
+			401 - Not authenticated
+			5*** - We ran into a server error
+			GET /slices/<id number>/gcode -> Serve file
+			GET /slices/<id number>/config -> config for job
+			POST|PUT /slices/<id number>/config
+				405 - Method not allowed
+				GET /slices/<id number>/stls/<stl_num>/ -> stl for the job
+				POST|PUT /slices/<id number>/stls
+					405 - Method not allowed
+					GET /slices/<id number/stls/ -> returns zip of all STLs for job
+
+					Job State
+					GET /slices/<id number>/state
+						200 - JSON current state | XML current state
+						PUT /slices/<id number>/state
+							200 - Entry into the DB was successful
 
 run(host='0.0.0.0', port=8080, debug=True)
 #bottle.run(server=bottle.CGIServer)
