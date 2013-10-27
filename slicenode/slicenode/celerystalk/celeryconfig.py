c47eb609194ae2c33a607d75ae152cd05ae656b9
Ross Hendrickson <ross@savorywatt.com>
2013-10-27 00:45:43 -0500

    Move models to external API. Node will not have DB.

--- slicenode/slicenode/celerystalk/celeryconfig.py
+++ slicenode/slicenode/celerystalk/celeryconfig.py
@@ -1,17 +1,9 @@
-#CELERY_RESULT_BACKEND = 'mongodb'
 BROKER_HOST = 'localhost'
 BROKER_PORT = 5672
 BROKER_TRANSPORT = 'amqp'
 BROKER_VHOST = 'test_host'
 BROKER_USER = 'test'
 BROKER_PASSWORD = 'test_pass'
-CELERY_MONGODB_BACKEND_SETTINGS = {
-    'host': 'localhost',
-    'port': 27017,
-    'database': 'testdb',
-#    'user': user,
-#    'password': password,
-    'taskmeta_collection':'teskmeta'}
 
 # Define routes
 """CELERY_ROUTES = {
--- slicenode/slicenode/celerystalk/priority_queue.sh
+++
@@ -1,2 +0,0 @@
-# Shell script to start up a task worker for priority slicers
-# Should run as a daemon
